49bb6dc86c0b8b44a13634e576e531540b852e16
dayiskuan <dayiskuan@gmail.com>
2016-06-26 17:18:14 -0500

    Second version

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,7 @@
 $(document).on("ready", function() { 
+	$("body").mCustomScrollbar({
+		theme:"minimal"
+	});
 	var trabajos = [];
 	var contador = 1;
 	$.ajax({
@@ -12,13 +15,22 @@ $(document).on("ready", function() {
 
 	var dibujar = function(datos, posicion) {
 		datos["grupo" + posicion].map(function(itemWork){
-			$("#myWork").fadeIn(3000).append("<li><a class='example-image-link' href='" + itemWork.imagen + "' data-lightbox='example-set' data-title='" + itemWork.name + itemWork.description + itemWork.url + "'><img class='example-image' src='" + itemWork.thumb + "' alt=''/></a></li>")
+			$("#myWork").append("<li><a class='example-image-link' href='" + itemWork.imagen + "' data-lightbox='example-set' data-title='" + itemWork.name + itemWork.description + itemWork.url + "'><img class='example-image' src='" + itemWork.thumb + "' alt=''/></a></li>")
 		})
 		contador = contador + 1;
 	} 
 
 	$("#showWork").click(function(){
 		dibujar(trabajos, contador);
+	})
+
+	$("#primaryMenu").click(function(){
+		$("#principalNav").fadeIn(500);
+		$("html").css("overflow", "hidden");
+	})
+	$("#closeNav").click(function(){
+		$("#principalNav").fadeOut(500)
+		$("html").css("overflow", "auto");
 	})
 
 })
